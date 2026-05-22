6b018bdbc9a79bf26bc6a84e82b9b1c28e560a8f
panipanda <tkashitsyna@gmail.com>
2026-05-22 13:58:20 +0200

    Change build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,6 @@ plugins {
 group = "org.proanima"
 version = "0.1.0"
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
