90450eb0bd6389ee343e8a04396c67f5fbd8ba99
panipanda <tkashitsyna@gmail.com>
2026-05-22 12:23:15 +0200

    Add build configuration files for Gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
+    application
+}
+
+group = "org.proanima"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("org.proanima.shelter.ApplicationKt")
+}
