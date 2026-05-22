e85266d4eeeca4f03c0dc51b14a4f12a4bbdb16e
panipanda <4282825+panipanda@users.noreply.github.com>
2026-05-22 17:46:56 +0200

    Add Ktor server setup and health check endpoint to Application

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,10 @@ version = "0.1.0"
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
+    implementation("io.ktor:ktor-server-core-jvm:2.3.12")
+    implementation("io.ktor:ktor-server-netty-jvm:2.3.12")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
     testImplementation(kotlin("test"))
 }
 
