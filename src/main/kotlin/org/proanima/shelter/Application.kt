3d0ef019b370fc9e33bd625223734aeb76f54451
panipanda <tkashitsyna@gmail.com>
2026-05-22 17:46:56 +0200

    Add Ktor server setup and health check endpoint to Application

--- src/main/kotlin/org/proanima/shelter/Application.kt
+++ src/main/kotlin/org/proanima/shelter/Application.kt
@@ -1,5 +1,26 @@
 package org.proanima.shelter
 
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+
 fun main() {
-    println("Shelter Volunteer application started")
+    embeddedServer(
+        factory = Netty,
+        port = 8080,
+        host = "0.0.0.0",
+        module = Application::module
+    ).start(wait = true)
+}
+
+fun Application.module() {
+    routing {
+        get("/health") {
+            call.respondText("OK")
+        }
+    }
 }
