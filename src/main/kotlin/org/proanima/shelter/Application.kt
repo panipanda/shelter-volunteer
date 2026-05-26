3cbb25f6a1b9e476dd40d00eb58fedb058177e50
panipanda <tkashitsyna@gmail.com>
2026-05-26 17:44:10 +0200

    Refactor application structure and add VisitRoutesTest for visit management

--- src/main/kotlin/org/proanima/shelter/Application.kt
+++ src/main/kotlin/org/proanima/shelter/Application.kt
@@ -2,16 +2,16 @@ package org.proanima.shelter
 
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.netty.Netty
 import io.ktor.server.routing.routing
-import io.ktor.server.http.content.staticResources
-import org.proanima.shelter.routes.healthRoutes
-import org.proanima.shelter.routes.catRoutes
-import org.proanima.shelter.service.CatService
 import org.proanima.shelter.repository.JsonCatRepository
-import org.proanima.shelter.routes.visitRoutes
-import org.proanima.shelter.service.VisitService
 import org.proanima.shelter.repository.JsonVisitRepository
+import org.proanima.shelter.routes.catRoutes
+import org.proanima.shelter.routes.healthRoutes
+import org.proanima.shelter.routes.visitRoutes
+import org.proanima.shelter.service.CatService
+import org.proanima.shelter.service.VisitService
 
 fun main() {
     embeddedServer(
@@ -23,9 +23,16 @@ fun main() {
 }
 
 fun Application.module() {
-    val catService = CatService(JsonCatRepository())
-    val visitService = VisitService(JsonVisitRepository())
+    configureRoutes(
+        catService = CatService(JsonCatRepository()),
+        visitService = VisitService(JsonVisitRepository())
+    )
+}
 
+fun Application.configureRoutes(
+    catService: CatService,
+    visitService: VisitService
+) {
     routing {
         healthRoutes()
         catRoutes(catService)
